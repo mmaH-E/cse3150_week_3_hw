4e45e4bb9144d2714bd96e17e7c4d76c202ec5a8
mmaH-E <ewanhamilton6@gmail.com>
2025-09-19 23:28:58 -0400

    HW 3 done

--- README.md
+++ README.md
@@ -1,1 +1,8 @@
 # cse3150_week_3_hw
+
+Pointers to manipulate the memory such as the GPAs of the students and to also access their information
+Const was used to ensure that the original value is not modified such as the student's names
+References used to access and manipulate values without copying it over
+Casting to convert strings into integers when the program was looking for an initial input
+Exceptions used to handle errors and handled with try - catch
+Control Flow we used switch statements to have different behaviors based on user input and for loops to handle iterating over a list
---
+++ solution.cpp
@@ -0,0 +1,135 @@
+#include <iostream>
+#include <cstring>   // for strlen, strcpy
+
+
+// TODO: function prototypes
+void addStudent(char* name, double gpa, char* names[], double gpas[], int& size, int capacity);
+void updateGPA(double* gpaPtr, double newGpa);
+void printStudent(const char* name, const double& gpa);
+double averageGPA(const double gpas[], int size);
+// TODO: implement addStudent
+void addStudent(char* name, double gpa, char* names[], double gpas[], int& size, int capacity){
+    if (size >= capacity){
+        throw "List full";
+    }
+    names[size] = name;
+    gpas[size] = gpa;
+    size ++;
+
+}
+// TODO: implement updateGPA
+void updateGPA(double* gpaPtr, double newGpa){
+    if (gpaPtr != nullptr) *gpaPtr = newGpa;
+
+}
+// TODO: implement printStudent
+void printStudent(const char* name, const double& gpa){
+    std::cout << name << std::endl;
+    std::cout << gpa << std::endl;
+}
+// TODO: implement averageGPA
+double averageGPA(const double gpas[], int size){
+    if (size == 0){
+        throw "No students";
+    }
+    double avg = 0;
+    for (int i = 0; i<size; i++){
+        avg += gpas[i];
+    }
+
+    return avg/size;
+}
+
+
+int main(int argc, char* argv[]) {
+    if (argc < 2) {
+        std::cout << "Usage: ./program <capacity>" << std::endl;
+        return 1;
+    }
+
+    int capacity = std::stoi(argv[1]);
+    char* names[capacity];
+    double gpas[capacity];
+    int size = 0;
+
+    int choice;
+    do {
+        std::cout << "Menu:\n";
+        std::cout << "1. Add student\n";
+        std::cout << "2. Update GPA\n";
+        std::cout << "3. Print all students\n";
+        std::cout << "4. Compute average GPA\n";
+        std::cout << "5. Quit\n";
+        std::cout << "Enter choice: ";
+        std::cin >> choice;
+
+        switch (choice) {
+            case 1: {
+                // TODO: implement menu logic
+
+                char buff[256];
+                double gpa;
+                std::cout << "Enter the student's name: " << std::endl;
+                std::cin >> buff;
+                std::cout << "Enter the student's gpa: " << std::endl;
+                std::cin >> gpa;
+                try {
+                    addStudent(buff, gpa, names, gpas, size, capacity);
+                    
+                } catch (const char* msg) {
+                    std::cout << msg << std::endl << std::endl;
+                }
+
+                break;
+            }
+            case 2: {
+                // TODO: implement menu logic
+                int idx;
+                double newgpa;
+                std::cout << "enter the index of the student and their new gpa: " << std::endl;
+                std::cin >> idx >> newgpa;
+
+                updateGPA(&gpas[idx], newgpa);
+
+
+                break;
+            }
+            case 3: {
+                // TODO: implement menu logic
+                for (int i = 0; i < size; i++){
+                    printStudent(names[i], gpas[i]);
+                }
+                break;
+            }
+            case 4: {
+                // TODO: implement menu logic
+                try {
+                    double avg = averageGPA(gpas, size);
+
+                    std::cout << "Average GPA" << std::endl;
+                    std::cout << avg << std::endl;
+                    
+                } 
+                catch (const char* msg) {
+                    std::cout << msg << std::endl;
+                }
+
+                break;
+            }
+            case 5: {
+                std::cout << "Goodbye!" << std::endl;
+                break;
+            }
+            default: {
+                std::cout << "Invalid choice" << std::endl;
+            }
+        }
+    } while (choice != 5);
+
+    // TODO: free memory
+    for (int i = 0; i < size; i++) delete[] names[i];
+    delete[] names;
+    delete[] gpas;
+
+    return 0;
+}
